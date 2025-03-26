d372179ff870a1abcdfba8b611e9f851f50ed358
Sonia Akhter <sonia798524@gmail.com>
2025-03-26 20:09:28 +0600

    filter,getByRole,getByText chaining method

--- tests/Locators.spec.js
+++ tests/Locators.spec.js
@@ -10,7 +10,12 @@ test('Playwright Special locators', async ({ page }) => {
     await page.getByLabel("Check me out if you Love IceCreams!").click()
     await page.getByLabel("Employed").check()
     await page.getByLabel("Gender").selectOption("Female")
-    
+    await page.getByPlaceholder("Password").fill("abc123")
+    await page.getByRole("button",{name:'Submit'}).click()
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible()
+    await page.getByRole("link",{name:'Shop'}).click()
+    await page.locator("app-card").filter({hasText:'Nokia Edge'}).getByRole("button").click()
+
 
 
 
